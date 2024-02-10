5e29e973323c3c655721655dc17a83221c67706b
polakDas <polak.cmed@gmail.com>
2024-02-10 19:15:00 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import linkedList.LinkedList;
+import queue.Queue;
+import stack.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        // Linked list
+//        LinkedList<String> linkedList = new LinkedList<>();
+//
+//        linkedList.add("Kazipara");
+//
+//        System.out.println(linkedList.removeFirst());
+//
+//        linkedList.add("Shewrapara");
+//
+//        System.out.println(linkedList.removeLast());
+//
+//        linkedList.addFirst("Taltola");
+//
+//        System.out.println(linkedList);
+
+
+        // Stack using linked list
+//        Stack<Integer> stack = new Stack<>();
+//
+//        stack.pushLL(10);
+//        stack.pushLL(20);
+//        stack.pushLL(30);
+//        stack.pushLL(34);
+//
+//        System.out.println(stack.sizeLL());
+//
+//        System.out.println(stack.peekLL());
+//        System.out.println(stack.popLL());
+//
+//        System.out.println(stack);
+
+
+        // Queue using linked list
+        Queue<Integer> queue = new Queue<>();
+
+        queue.enqueue(90);
+        queue.enqueue(190);
+        queue.enqueue(290);
+
+        System.out.println(queue.dequeue());
+
+        System.out.println("Peek: " + queue.peek());
+
+        queue.enqueue(390);
+
+        System.out.println("Size: " + queue.size());
+        System.out.println(queue);
+    }
+}
